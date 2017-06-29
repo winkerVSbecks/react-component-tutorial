a1aba6a4fa1174c114a85bd251c9f98b938c2b6a
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2017-06-29 16:58:33 -0400

    work in progress

--- exercise-app.js
+++ exercise-app.js
@@ -1,50 +1,68 @@
-const API_KEY = '5cc1f25759881907aed6171543839b19';
+const API_KEY = '498beb84557a3e0a4b7b519c6c1134fa';
 
 function getWeather() {
   return fetch(`http://api.openweathermap.org/data/2.5/weather?id=6167865&appid=${API_KEY}&units=metric`)
     .then(res => res.json())
     .then(reshapeData);
-};
+}
 
 function reshapeData(data) {
-  ReactDOM.render(<Weather
-    temp={data.main.temp}
-    description={data.weather[0].description}
-    icon={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-  />,
+  ReactDOM.render(<Weather {...data} />,
   document.getElementById('exercise'));
-  setTimeout(() => {
-    console.log('curr temp: ', data.main.temp)
+  return data;
+}
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temp: props.main.temp,
+      description: props.weather[0].description,
+      icon: `http://openweathermap.org/img/w/${props.weather[0].icon}.png`,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(
+      () => this.updateWeather(),
+      15000
+    );
+  }
+
+  updateWeather() {
     getWeather();
-  }, 1000);
-}
+  }
 
-const Weather = ({ temp, description, icon }) => (
-  <div className="bw2 ba blue dib pa3 bg-washed-blue">
-    <div className="flex items-stretch mb2">
-      <img src={icon} />
-      <header className="ml3">
-        <h1 className="f7 ttu tracked mt0 mb2 flex-auto dark-blue">Toronto</h1>
-        <p className="f3 mv0">{ temp } ℃</p>
-      </header>
+  render() {
+    return (
+    <div className="bw2 ba blue dib pa3 bg-washed-blue">
+
+      <div className="flex items-stretch mb2">
+        <img src={this.state.icon} />
+
+        <header className="ml3">
+          <h1 className="f7 ttu tracked mt0 mb2 flex-auto dark-blue">Toronto</h1>
+          <p className="f3 mv0">{ this.state.temp } ℃</p>
+        </header>
+      </div>
+
+      <p className="mv0 f5 ttc">{ this.state.description }</p>
     </div>
-    <p className="mv0 f5 ttc">{ description }</p>
-  </div>
-);
-
-const App = () => (
-  <Weather
-    temp="N/A"
-    description="Information unavailable"
-    icon="http://openweathermap.org/img/w/10d.png"
-  />
-);
-
-const componentDidMount = () => {
-  getWeather();
+    )
+  }
 }
 
-componentDidMount();
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      data: getWeather(),
+    }
+  },
+  render: function() {
+    return (
+      <Weather />
+      )
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('exercise'));
-
