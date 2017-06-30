df73860ba26273c4b73373a75aa7ed736ba1a596
cgolinski <cgolinski@users.noreply.github.com>
2017-06-30 14:43:56 -0400

    solution

--- exercise-app.js
+++ exercise-app.js
@@ -1,15 +1,21 @@
 const API_KEY = '5cc1f25759881907aed6171543839b19';
 
 function getWeather() {
+  console.log('getWeather is being called');
   return fetch(`http://api.openweathermap.org/data/2.5/weather?id=6167865&appid=${API_KEY}&units=metric`)
     .then(res => res.json())
     .then(reshapeData);
 }
 
-function reshapeData() {
-
+function reshapeData(data) {
+  return {
+    temp: data.main.temp,
+    description: data.weather[0].description,
+    icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+  };
 }
 
+//somewhere, setTimeout and then call getWeather
 
 const Weather = ({ temp, description, icon }) => (
   <div className="bw2 ba blue dib pa3 bg-washed-blue">
@@ -28,14 +34,39 @@ const Weather = ({ temp, description, icon }) => (
 );
 
 
-const App = () => (
-  <Weather
-    temp={14.85}
-    description="proximity shower rain"
-    icon="http://openweathermap.org/img/w/10d.png"
-  />
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      data: null,
+    };
+  }
 
+  refreshWeatherState() {
+    getWeather()
+      .then(data => this.setState({ data }));
+  }
+
+  componentDidMount() {
+    this.refreshWeatherState();
+
+    setInterval(this.refreshWeatherState.bind(this), 15000);
+  }
+
+  render() {
+    if (this.state.data == null) {
+      return null;
+    }
+
+    return (
+      <Weather
+        temp={this.state.data.temp}
+        description={this.state.data.description}
+        icon={this.state.data.icon}
+      />
+    );
+  };
+};
 
 ReactDOM.render(<App />, document.getElementById('exercise'));
